96658bba16af95e933274946c37f267444168cf0
laser54 <Z6X84xU4Ir2U>
2023-01-05 22:10:06 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# chatgpt_to_xlsx
-Save list of chatgpt answers to xlxs file
+# Chatgpt to xlsx
+This project is a simple command-line client for interacting with the OpenAI API. It reads in a list of questions from an input file, generates responses for each question using the OpenAI API, and writes the results to an Excel file.
+
+## Prerequisites
+You will need to have a valid OpenAI API key. You can sign up for an API key at the OpenAI website.
+
+This project uses the openai and openpyxl libraries, which can be installed using pip:
+
+Copy code 
+``` 
+pip install -r requirements.txt
+```
+## Usage
+Save your OpenAI API key in a file called .env in the root directory of this project. The file should contain a single line with the following format:
+Copy code
+```
+API_KEY=your-api-key-here
+```
+Create an input file called input.txt in the root directory of this project. This file should contain a list of questions, with one question per line.
+
+## Run the script:
+
+Copy code
+```
+python main.py
+```
+The script will read in the list of questions from the input file, generate responses for each question using the OpenAI API, and write the results to an Excel file called results.xlsx.
---
+++ main.py
@@ -0,0 +1,68 @@
+import openai
+import json
+import openpyxl
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv('API_KEY')
+
+def get_answer(question):
+    """
+    Use the OpenAI API to generate a response to a question.
+
+    Parameters:
+    - question: The question to generate a response for.
+      (type: str)
+
+    Returns:
+    - A string containing the response to the question.
+      (type: str)
+    """
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=question,
+            max_tokens=2048,
+            temperature=0.5,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        json_string = json.dumps(response)
+        data = json.loads(json_string)
+        return data["choices"][0]["text"]
+    except Exception as e:
+        return "ERROR"
+
+
+with open('input.txt', 'r') as input_file:
+    questions = input_file.readlines()
+
+# Create an empty list to store the results
+results = []
+
+# Iterate over the questions and call the some_func() function for each question, storing the results in the results list
+for question in questions:
+    result = get_answer(question).lstrip()
+    if result != "ERROR":
+        print('parsing .... ' + question)
+    else:
+        print('SOMETHING WRONG WITH .... ' + question)
+    results.append([question, result])
+
+# Open the Excel file in write mode and create a new worksheet
+wb = openpyxl.Workbook()
+ws = wb.active
+
+# Iterate over the results and write each row to the worksheet
+for row in results:
+    ws.append(row)
+
+# Save the Excel file
+wb.save('results.xlsx')
+
+
+
